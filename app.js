f05ce866ab362ad91fd339cb5c213c4a535df37a
swillrodt <sumnerwillrodt@gmail.com>
2021-10-06 22:30:40 -0700

    generate webpage

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+//const name = profileDataArgs[0];
+//const github = profileDataArgs[1];
 
-const printProfilData = profileDataArr => {
-    //This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+const [name, github] = profileDataArgs;
 
-    console.log('================');
-
-    // ... Is the same as this.
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfilData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
